150699aca00757d0be32ed0be3ec503569673508
Ricadon5 <ricardo_wilson95@aol.com>
2024-11-09 23:41:34 +0100

    Version 4

--- Depot.py
+++ Depot.py
@@ -1,3 +1,6 @@
+from statistics import quantiles
+
+
 class Ingredient:
     def __init__(self, nom, quantite=0, prix=0, unite="unité"):
         self.nom = nom
@@ -10,6 +13,19 @@ class Ingredient:
         self.quantite += a
 
 
+class Facture(Ingredient):
+    def __init__(self, nom, quantite, prix, unite, total, n_facture=0, tva=0.15):
+        super().__init__(nom, quantite, prix, unite)
+        self.total = total
+        self.n_facture = n_facture
+        self.tva = tva
+
+    # Metodo calcolo prezzo senza HTVA
+    def nouvelle_facture(self):
+        self.total = self. quantite * self.prix * self.tva
+        self.n_facture += self.n_facture
+        print(self.total, self.n_facture)
+
 produit_1 = Ingredient("Bouteille", 0, 5, "unité")
 produit_2 = Ingredient("Sucre de canne", 0, 0.07, "g")
 produit_3 = Ingredient("Concentré du jus d'orange", 0, 0.3, "cl")
@@ -19,8 +35,12 @@ produit_6 = Ingredient("Acide citrique", 0, 0.7, "cL")
 produit_7 = Ingredient("Etiquette", 0, 0.3,"unité")
 produit_8 = Ingredient("Benzoate de sodium", 0, 0.4,"g")
 
-produit_3.actualiser_la_quantite(100)
+produit_1.actualiser_la_quantite(20)
 print(produit_1.nom)
+
+
+
+
 
 
 
