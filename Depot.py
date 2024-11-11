c6983dc1a12d6516fdf9713d1faa277836179c9c
Ricadon5 <ricardo_wilson95@aol.com>
2024-11-11 16:37:39 +0100

    Version 5

--- Depot.py
+++ Depot.py
@@ -1,7 +1,4 @@
-from statistics import quantiles
-
-
-class Ingredient:
+class Produit:
     def __init__(self, nom, quantite=0, prix=0, unite="unité"):
         self.nom = nom
         self.quantite = quantite
@@ -13,8 +10,8 @@ class Ingredient:
         self.quantite += a
 
 
-class Facture(Ingredient):
-    def __init__(self, nom, quantite, prix, unite, total, n_facture=0, tva=0.15):
+class Facture(Produit):
+    def __init__(self, nom, quantite, prix, unite, total = 0, n_facture=0, tva=0.15):
         super().__init__(nom, quantite, prix, unite)
         self.total = total
         self.n_facture = n_facture
@@ -22,21 +19,18 @@ class Facture(Ingredient):
 
     # Metodo calcolo prezzo senza HTVA
     def nouvelle_facture(self):
-        self.total = self. quantite * self.prix * self.tva
-        self.n_facture += self.n_facture
+        self.total = (self. quantite * self.prix * self.tva) + self. quantite * self.prix
+        self.n_facture += 1
         print(self.total, self.n_facture)
 
-produit_1 = Ingredient("Bouteille", 0, 5, "unité")
-produit_2 = Ingredient("Sucre de canne", 0, 0.07, "g")
-produit_3 = Ingredient("Concentré du jus d'orange", 0, 0.3, "cl")
-produit_4 = Ingredient("Eau pétillante", 0, 0.15, "L")
-produit_5 = Ingredient("Colorant orange", 0, 0.7,"cL")
-produit_6 = Ingredient("Acide citrique", 0, 0.7, "cL")
-produit_7 = Ingredient("Etiquette", 0, 0.3,"unité")
-produit_8 = Ingredient("Benzoate de sodium", 0, 0.4,"g")
-
-produit_1.actualiser_la_quantite(20)
-print(produit_1.nom)
+produit_1 = Produit("Bouteille", 0, 5, "unité")
+produit_2 = Produit("Sucre de canne", 0, 0.07, "g")
+produit_3 = Produit("Concentré du jus d'orange", 0, 0.3, "cl")
+produit_4 = Produit("Eau pétillante", 0, 0.15, "L")
+produit_5 = Produit("Colorant orange", 0, 0.7,"cL")
+produit_6 = Produit("Acide citrique", 0, 0.7, "cL")
+produit_7 = Produit("Etiquette", 0, 0.3,"unité")
+produit_8 = Produit("Benzoate de sodium", 0, 0.4,"g")
 
 
 
--- Village.py
+++ Village.py
@@ -100,7 +100,7 @@ frame_1 = customtkinter.CTkFrame(village, fg_color="#143d59", corner_radius=0, h
 frame_2 = customtkinter.CTkFrame(village, fg_color="#f4b41a", height=800, width=1000)
 frame_3 = customtkinter.CTkFrame(frame_2, fg_color="ivory", corner_radius=20, height=400, width=600,
                                  bg_color="#f4b41a", border_color="#143d59", border_width=10)
-frame_4 = customtkinter.CTkFrame(frame_2, fg_color="#143d59", corner_radius=20, height=200, width=600,
+frame_4 = customtkinter.CTkFrame(frame_2, fg_color="#143d59", corner_radius=20, height=150, width=600,
                                  bg_color="#f4b41a", border_color="ivory", border_width=5)
 frame_1.pack(ipadx=10, ipady=10, expand=True, fill=BOTH, side=RIGHT)
 frame_2.pack(ipadx=10, ipady=10, expand=True, fill=BOTH)
@@ -110,13 +110,24 @@ frame_4.place(relx=0.5, rely=0.2, anchor="center")
 
 
 # Labels per inserire del testo nel frame_4
-titre = customtkinter.CTkLabel(frame_4, text = "Bienvenue au village", font = style_1, text_color="ivory")
-titre.place(relx = 0.05, rely = 0.2, anchor = "w")
+titre_41 = customtkinter.CTkLabel(frame_4, text = "Bienvenue à Bruxville", font = style_1, text_color="ivory")
+titre_42 = customtkinter.CTkLabel(frame_4, text = "Da qui poi raggiungere la banca, il negozio oppure la fabbrica", font = style_2, text_color="ivory")
+titre_41.place(relx = 0.05, rely = 0.3, anchor = "w")
+titre_42.place(relx = 0.05, rely = 0.6, anchor = "w")
 
 
+# Aggiunge immagini al frame_3
+photo_31 = customtkinter.CTkImage(light_image=Image.open("./Images/Arrow.png"),
+                                         size=(25, 25))
 # Labels per inserire del testo nel frame_3
 information = customtkinter.CTkLabel(frame_3, text = "Se ti senti perso clicca qui", font = style_2, text_color="#143d59")
-information.place(relx = 0.05, rely = 0.2, anchor = "w")
+batiment = customtkinter.CTkLabel(frame_3, text = "Segli l'edificio dove vuoi andare", font = style_2, text_color="#143d59")
+arrow = customtkinter.CTkLabel(frame_3, image=photo_31, text=" ", font=style_3)
+arrow_2 = customtkinter.CTkLabel(frame_3, image=photo_31, text=" ", font=style_3)
+information.place(relx = 0.15, rely = 0.2, anchor = "w")
+batiment.place(relx = 0.15, rely = 0.4, anchor = "w")
+arrow.place(relx=0.08, rely=0.2, anchor="center")
+arrow_2.place(relx=0.08, rely=0.4, anchor="center")
 # Aggiunge i bottoni del frame_3
 info = customtkinter.CTkButton(frame_3, text="Info", text_color="#143d59", fg_color="#f4b41a", font=style_2,
                                command=ouvrir_info_window)
@@ -127,7 +138,7 @@ info.place(relx=0.8, rely=0.2, anchor="center")
 # Labels per inserire del testo
 compte_bancaire = customtkinter.CTkLabel(frame_1, text = "Denaro a disposizione", font = style_2, text_color="ivory")
 orange_bull = customtkinter.CTkLabel(frame_1, text = "Orange Bull fabbricate", font = style_2, text_color="ivory")
-titre_2 = customtkinter.CTkLabel(frame_1, text = "Materiali", font = style_1, text_color="ivory")
+titre_11 = customtkinter.CTkLabel(frame_1, text = "Produits", font = style_1, text_color="ivory")
 ingredientes = customtkinter.CTkLabel(frame_1, text = f"\n{produit_1.nom} : {produit_1.quantite} {produit_1.unite}\n"
                                                       f"\n{produit_2.nom} : {produit_2.quantite} {produit_2.unite}\n"
                                                       f"\n{produit_3.nom} : {produit_3.quantite} {produit_3.unite}\n"
@@ -139,8 +150,8 @@ ingredientes = customtkinter.CTkLabel(frame_1, text = f"\n{produit_1.nom} : {pro
                                       font = style_2, text_color="ivory", justify="left")
 compte_bancaire.place(relx = 0.1, rely = 0.1, anchor = "w")
 orange_bull.place(relx = 0.1, rely = 0.2, anchor = "w")
-titre_2.place(relx = 0.1, rely = 0.35, anchor = "w")
-ingredientes.place(relx = 0.1, rely = 0.5, anchor = "w")
+titre_11.place(relx = 0.1, rely = 0.35, anchor = "w")
+ingredientes.place(relx = 0.1, rely = 0.6, anchor = "w")
 # Progressbar
 progressbar_1 = customtkinter.CTkProgressBar(frame_1, orientation="horizontal", fg_color="ivory", progress_color="#f4b41a")
 progressbar_2 = customtkinter.CTkProgressBar(frame_1, orientation="horizontal", fg_color="ivory", progress_color="#f4b41a")
